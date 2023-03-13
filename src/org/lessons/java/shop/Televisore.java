82c3a0ceefeea25cdf2821f33d898bae18f2d52e
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-13 15:06:59 +0100

    Add:Televisore class

---
+++ src/org/lessons/java/shop/Televisore.java
@@ -0,0 +1,34 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Televisore extends Prodotto{
+//    ATTRIBUTES
+    private boolean[] isSmart = {true,false};
+
+    private boolean smart;
+    private double size;
+
+    Random randomizer = new Random();
+    int randomNumber = randomizer.nextInt(0,2);
+    Televisore(){
+        smart = isSmart[randomNumber];
+        size = 6.01;
+    }
+
+    public boolean getSmart() {
+        return smart;
+    }
+
+    public void setSmart(boolean smart) {
+        this.smart = smart;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+}
